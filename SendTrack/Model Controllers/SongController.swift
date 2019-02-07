42da05af6987d06bff51d3caffade7711d182793
Steve Lederer <stevieled@gmail.com>
2019-02-07 11:44:08 -0700

    adds CupertinioJWT pod

--- SendTrack/Model Controllers/SongController.swift
+++ SendTrack/Model Controllers/SongController.swift
@@ -17,7 +17,7 @@ class SongController {
      This function takes my clientID and clientSecret from the APIKeys.plist file and retrieves the Access Token needed for further interaction with the Spotify API.
      
      - Returns: This function completes with a string of the desired token
- */
+     */
     static func getSpotifyAccessToken(completion: @escaping(String?) -> Void) {
         guard let spotifyBasicAPIKey = spotifyBasicAPIKey() else { return }
         
@@ -28,23 +28,23 @@ class SongController {
             "Postman-Token": "f79e695c-5009-4abe-b128-2e8206060816"
         ]
         
-        let postData = NSMutableData(data: "grant_type=client_credentials".data(using: String.Encoding.utf8)!)
+        let body =
+            "grant_type=client_credentials".data(using: .utf8)!
         
-        postData.append("&undefined=undefined".data(using: String.Encoding.utf8)!)
-        
-        let request = NSMutableURLRequest(url: NSURL(string: "https://accounts.spotify.com/api/token")! as URL,
-                                          cachePolicy: .useProtocolCachePolicy,
-                                          timeoutInterval: 10.0)
+        var request = URLRequest(url: URL(string: "https://accounts.spotify.com/api/token")!,
+                                 cachePolicy: .useProtocolCachePolicy,
+                                 timeoutInterval: 10.0)
         request.httpMethod = "POST"
         request.allHTTPHeaderFields = headers
-        request.httpBody = postData as Data
+        request.httpBody = body
         
         let session = URLSession.shared
-        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, _, error) -> Void in
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
             if let error = error {
                 print(error.localizedDescription)
                 completion(nil) ; return
             }
+            print(response)
             
             guard let data = data else { completion(nil) ; return }
             do {
@@ -60,26 +60,26 @@ class SongController {
         
     }
     
-
     
-//    static func fetchSpotifySongs(with searchTerm: String, completion: @escaping ([SpotifyTrack]?) -> Void) {
-//        guard let url = baseSpotifyURL?.appendingPathComponent("v1").appendingPathComponent("search") else { completion(nil) ; return }
-//        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
-//        let searchTermQueryItem = URLQueryItem(name: "q", value: searchTerm)
-//        let typeQueryItem = URLQueryItem(name: "type", value: "track")
-//        components?.queryItems = [searchTermQueryItem, typeQueryItem]
-//        
-//        guard let requestURL = components?.url else { completion(nil) ; return }
-//        
-//        let headers = [
-//            "Authorization" : "Bearer \(spotifyToken)",
-//        ]
-//        
-//        
-//        print("📡📡📡 Spotify URL: \(requestURL.absoluteString)")
-//        
-//        let request = URLRequest(url: requestURL)
-//        
-//        
-//    }
+    
+    //    static func fetchSpotifySongs(with searchTerm: String, completion: @escaping ([SpotifyTrack]?) -> Void) {
+    //        guard let url = baseSpotifyURL?.appendingPathComponent("v1").appendingPathComponent("search") else { completion(nil) ; return }
+    //        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+    //        let searchTermQueryItem = URLQueryItem(name: "q", value: searchTerm)
+    //        let typeQueryItem = URLQueryItem(name: "type", value: "track")
+    //        components?.queryItems = [searchTermQueryItem, typeQueryItem]
+    //
+    //        guard let requestURL = components?.url else { completion(nil) ; return }
+    //
+    //        let headers = [
+    //            "Authorization" : "Bearer \(spotifyToken)",
+    //        ]
+    //
+    //
+    //        print("📡📡📡 Spotify URL: \(requestURL.absoluteString)")
+    //
+    //        let request = URLRequest(url: requestURL)
+    //
+    //
+    //    }
 }
