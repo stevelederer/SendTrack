2073da6c1ef9484b622b86d24cc6bfb3d2f141db
Steve Lederer <stevieled@gmail.com>
2019-02-09 17:17:30 -0700

    updates uicolor helper, apple music search, fetching apple music artwork, ui changes, ui now functions for searching and details

--- SendTrack/Helpers/HexColor.swift
+++ SendTrack/Helpers/HexColor.swift
@@ -24,4 +24,23 @@ extension UIColor {
             blue: rgb & 0xFF
         )
     }
+    
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 0
+        
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, alpha: 1
+        )
+    }
 }
--- SendTrack/View Controllers/SongSearchTableViewController.swift
+++ SendTrack/View Controllers/SongSearchTableViewController.swift
@@ -15,47 +15,104 @@ class SongSearchTableViewController: UITableViewController {
     var songs: [SteveSong] = []
     
     // MARK: - View Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        definesPresentationContext = true
+        setupNavBar()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
+    func setupNavBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.autocapitalizationType = .words
+        searchController.searchBar.autocorrectionType = .yes
+        searchController.searchBar.placeholder = "Search for a song..."
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return songs.count
     }
-
-    /*
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "songCell", for: indexPath) as! SongTableViewCell
+        let song = songs[indexPath.row]
+        cell.song = song
         return cell
     }
-    */
-
-    /*
+    
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "toSongDetailView" {
+            guard let indexPath = tableView.indexPathForSelectedRow,
+                let destinationVC = segue.destination as? SongDetailViewController else { return }
+            let song = songs[indexPath.row]
+            destinationVC.song = song
+        }
     }
-    */
+    
+    
+}
 
+extension SongSearchTableViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let searchTerm = searchBar.text ?? ""
+        self.dismiss(animated: true, completion: nil)
+        
+        AppleMusicController.fetchAppleMusicSongs(with: searchTerm) { (songs) in
+            guard let fetchedSongs = songs else { return }
+            var steveSongs: [SteveSong] = []
+            for song in fetchedSongs {
+                if let newSong = SteveSong(appleSong: song) {
+                    steveSongs.append(newSong)
+                }
+            }
+            self.songs = steveSongs
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.navigationItem.hidesSearchBarWhenScrolling = true
+                searchBar.text = nil
+            }
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let searchTerm = searchBar.text ?? ""
+        
+        AppleMusicController.fetchAppleMusicSongs(with: searchTerm) { (songs) in
+            guard let fetchedSongs = songs else { return }
+            var steveSongs: [SteveSong] = []
+            for song in fetchedSongs {
+                if let newSong = SteveSong(appleSong: song) {
+                    steveSongs.append(newSong)
+                }
+            }
+            self.songs = steveSongs
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+                self.navigationItem.hidesSearchBarWhenScrolling = true
+            }
+        }
+    }
+    
 }
